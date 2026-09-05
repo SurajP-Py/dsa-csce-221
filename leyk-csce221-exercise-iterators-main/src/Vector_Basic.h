7f9d51bb054af1b6df59c478f2cbc4b238e81055
surajp-py <surajp0604@gmail.com>
2026-09-04 22:20:20 -0500

    iterator quiz first submission

--- leyk-csce221-exercise-iterators-main/src/Vector_Basic.h
+++ leyk-csce221-exercise-iterators-main/src/Vector_Basic.h
@@ -22,11 +22,15 @@ public:
     }
 
     // Should return an iterator pointing to the first element
-    iterator begin() noexcept { /* TODO */ }
+    iterator begin() noexcept { 
+        return iterator(array);
+     }
 
     // Should return an iterator pointing past the last element of the container
     // This is because you want to stop when you reach end
-    iterator end() noexcept { /* TODO */ }
+    iterator end() noexcept { 
+        return iterator(array + _size);
+     }
 
     // Iterator class for the Vector_Basic
     // It is essentially a pointer defined for the type with memory safety
@@ -44,48 +48,72 @@ public:
         // Points to some element in the vector (or nullptr)
         T* _ptr;
     public:
-        iterator() { /* TODO */ }
-        explicit iterator(T* ptr) { /* TODO */ }
+        iterator() : _ptr(nullptr) { /* TODO */ }
+        explicit iterator(T* ptr) : _ptr(ptr) { /* TODO */ }
 
         // This assignment operator is done for you, please do not add more
         iterator& operator=(const iterator&) noexcept = default;
 
         // This is used to access the element the iterator points to, like dereferencing a pointer
-        [[nodiscard]] reference operator*() const noexcept { /* TODO */ }
+        [[nodiscard]] reference operator*() const noexcept { return *_ptr; }
 
         // This is used to access data members of the underlying elements
         // It should return the address of the element the iterator points to
-        [[nodiscard]] pointer operator->() const noexcept { /* TODO */ }
+        [[nodiscard]] pointer operator->() const noexcept { return _ptr; }
 
         // Prefix Increment: ++a
         // The value returned should be the iterator moved forward in the container
-        iterator& operator++() noexcept { /* TODO */ }
+        iterator& operator++() noexcept { 
+            ++_ptr;
+            return *this;
+        }
 
         // Postfix Increment: a++
         // The value returned should be the iterator before it was moved forward in the container
-        iterator operator++(int) noexcept { /* TODO */ }
+        iterator operator++(int) noexcept { 
+            iterator tmp = *this;
+            ++_ptr;
+            return tmp;
+        }
 
         // Prefix Decrement: --a
         // The value returned should be the iterator moved backwards in the container
-        iterator& operator--() noexcept { /* TODO */ }
+        iterator& operator--() noexcept { 
+            --_ptr;
+            return *this;
+        }
 
         // Postfix Decrement: a--
         // The value returned should be the iterator before it was moved backwards in the container
-        iterator operator--(int) noexcept { /* TODO */ }
+        iterator operator--(int) noexcept { 
+            iterator tmp = *this;
+            --_ptr;
+            return tmp;
+        }
 
         // Should move the iterator forward by an offset and return the new iterator
-        iterator& operator+=(difference_type offset) noexcept { /* TODO */ }
+        iterator& operator+=(difference_type offset) noexcept { 
+            _ptr += offset;
+            return *this;
+        }
         
         // Should return a copy of the iterator moved forward by an offset
         // NOTE: The iterator itself should not be moved, only the copy
-        [[nodiscard]] iterator operator+(difference_type offset) const noexcept { /* TODO */ }
+        [[nodiscard]] iterator operator+(difference_type offset) const noexcept { 
+            return iterator(_ptr + offset);
+        }
         
         // Should move the iterator backwards by an offset and return the new iterator
-        iterator& operator-=(difference_type offset) noexcept { /* TODO */ }
+        iterator& operator-=(difference_type offset) noexcept { 
+            _ptr -= offset;
+            return *this;
+        }
 
         // Should return a copy of the iterator moved backwards by an offset
         // NOTE: The iterator itself should not be moved, only the copy
-        [[nodiscard]] iterator operator-(difference_type offset) const noexcept { /* TODO */ }
+        [[nodiscard]] iterator operator-(difference_type offset) const noexcept { 
+            return iterator(_ptr - offset);
+        }
 
         // Should return the difference between two iterators. This should be the offset betweent the
         // elements in memory.
@@ -98,12 +126,12 @@ public:
         // NOTE: This should be used to compare the iterators, not the elements they point to
         // HINT: Because iterators mimic pointers, many if not all of these operations can be done
         //       on the underlying pointers
-        [[nodiscard]] bool operator==(const iterator& rhs) const noexcept { /* TODO */ }
-        [[nodiscard]] bool operator!=(const iterator& rhs) const noexcept { /* TODO */ }
-        [[nodiscard]] bool operator<(const iterator& rhs) const noexcept { /* TODO */ }
-        [[nodiscard]] bool operator>(const iterator& rhs) const noexcept { /* TODO */ }
-        [[nodiscard]] bool operator<=(const iterator& rhs) const noexcept { /* TODO */ }
-        [[nodiscard]] bool operator>=(const iterator& rhs) const noexcept { /* TODO */ }
+        [[nodiscard]] bool operator==(const iterator& rhs) const noexcept { return _ptr == rhs._ptr; }
+        [[nodiscard]] bool operator!=(const iterator& rhs) const noexcept { return _ptr != rhs._ptr; }
+        [[nodiscard]] bool operator<(const iterator& rhs) const noexcept { return _ptr < rhs._ptr; }
+        [[nodiscard]] bool operator>(const iterator& rhs) const noexcept { return _ptr > rhs._ptr; }
+        [[nodiscard]] bool operator<=(const iterator& rhs) const noexcept { return _ptr <= rhs._ptr; }
+        [[nodiscard]] bool operator>=(const iterator& rhs) const noexcept { return _ptr >= rhs._ptr; }
     };
 };
 
